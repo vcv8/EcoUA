a4f87af654e10479123783e0ba775c466bcd4fa5
vcv8 <vcv183@gmail.com>
2019-06-04 10:45:58 +0200

    Inicio repositorio con proyecto de unity y otros

---
+++ ecoUAproject/Assets/tests/Scrpits/ElemBasura.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElemBasura : MonoBehaviour
+{
+    public Tipo tipo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Soy Basura de tipo " + tipo.colortipo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
